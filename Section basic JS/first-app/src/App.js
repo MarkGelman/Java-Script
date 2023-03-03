ea3560ad3e082bf2397e3c0e3b2822fe946e2c08
Mark <markgora1974@gmail.com>
2023-03-03 12:20:06 +0200

    27/02/23

--- Section basic JS/first-app/src/App.js
+++ Section basic JS/first-app/src/App.js
@@ -1,11 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
-import Counter from './components/Counter';
+import ChangeBorderByRange from './Homework/ChangeBorderByRange';
 
 function App() {
   return (
     <div>
-     <Counter />
+     <ChangeBorderByRange>
     </div>
   );
 }
