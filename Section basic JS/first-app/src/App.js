0d7a215d361d413d98af9abc92bf40c908d4d6cf
Mark <markgora1974@gmail.com>
2023-03-05 17:24:54 +0200

    start from A to Z

--- Section basic JS/first-app/src/App.js
+++ Section basic JS/first-app/src/App.js
@@ -1,11 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
-import ChangeBorderByRange from './Homework/ChangeBorderByRange';
+
+import React, {useState} from 'react';
+import ClassCounter from './ReactFrom_A_To_Z/ClassCounter';
+import Counter from './ReactFrom_A_To_Z/Counter';
 
 function App() {
+
   return (
-    <div>
-     <ChangeBorderByRange>
+    <div className="App">
+      <ClassCounter/>
     </div>
   );
 }
