41cbd08422b0d4c07953db788cdbfd66bc0f72c9
Mark <markgora1974@gmail.com>
2023-03-24 12:15:15 +0300

    React_HuckerU_start

--- basic_JS/first-app/src/App.js
+++ basic_JS/first-app/src/App.js
@@ -5,14 +5,17 @@ import React, { useState } from 'react';
 import PostList from './ReactFrom_A_To_Z/PostList';
 import './App.css'
 import AddNewPost from './ReactFrom_A_To_Z/AddNewPost';
+import MySelect from './ReactFrom_A_To_Z/UI/select/MySelect';
 
 
 function App() {
   const [posts, setPosts] = useState([
-    { id: 1, title: 'Javascript', body: 'Description' },
-    { id: 2, title: 'Javascript', body: 'Description' },
-    { id: 3, title: 'Javascript', body: 'Description' },
+    { id: 1, title: 'aa', body: 'яя' },
+    { id: 2, title: 'бб', body: 'ээ' },
+    { id: 3, title: 'вв', body: 'юю' },
   ])
+
+  const [selectedSort, setSelectedSort]= useState('')
 
   const createPost = (newPost) => {
     setPosts([...posts, newPost])
@@ -22,10 +25,27 @@ function App() {
     setPosts(posts.filter(p => p.id != post.id))
   }
 
+  const sortPosts = (sort) => {
+      setSelectedSort(sort);
+
+  }
+
   return (
 
     <div className="App">
       <AddNewPost create={createPost} />
+      <hr style={{margin: '15px 0'}}/>
+      <div>
+        <MySelect
+          value = {selectedSort}
+          onChange = {sortPosts}
+          defaultValue = "Сортировка"
+          options={[
+            {value: 'title', name: 'По названию'},
+            {value: 'body', name: 'По описанию'}
+          ]}
+          />
+      </div>
       {posts.length != 0
         ?
         <PostList remove={removePost} listPosts={posts} title="Посты про JS" />
