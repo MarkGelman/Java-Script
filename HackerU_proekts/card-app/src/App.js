1cc0249e97dac51e5b3608b7669a378348b5260d
MarkGelman <markgora1974@gmail.com>
2023-07-07 19:49:20 +0300

    start

---
+++ HackerU_proekts/card-app/src/App.js
@@ -0,0 +1,16 @@
+
+import { Image } from '@mui/icons-material';
+import './App.css';
+import Layout from './layout/Layout';
+import Router from './routes/Router';
+import { BrowserRouter } from 'react-router-dom';
+
+function App() {
+  return (
+    <>
+      
+    </>
+  );
+}
+
+export default App;
